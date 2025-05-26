2e122a5d6adc27e3876b2660ddc99de82602bb7d
Annatar <stijnjongbloed@hotmail.com>
2025-05-26 15:47:06 +0200

    Terug naar oude built structuur, moet built errors stoppen?

--- scaling/main.cpp
+++ scaling/main.cpp
@@ -1,8 +1,17 @@
-#include <iostream>
 #include <ROOT/RNTupleReader.hxx>
+#include <ROOT/RNTupleWriteOptions.hxx>
+#include <ROOT/RNTupleWriter.hxx>
+#include <ROOT/RNTuple.hxx>
+// #include <ROOT/RNTupleInspector.hxx>
+#include <iostream>
 
 #include <ROOT/RNTupleUtil.hxx>
 #include <ROOT/RNTupleView.hxx>
+#include <TCanvas.h>
+#include <TH1I.h>
+#include <TROOT.h>
+#include <TString.h>
+
 #include <TCanvas.h>
 #include <TH1I.h>
 #include <TROOT.h>
@@ -14,14 +23,13 @@
 #include <cstdio>
 #include <fstream>
 #include <iostream>
-#include <nlohmann/json.hpp>
 #include <ostream>
+#include <stdexcept>
 #include <string>
 #include <string_view>
 #include <utility>
 #include <variant>
 #include <vector>
-using json = nlohmann::json;
 
 using FVec = std::variant<std::vector<std::string>, std::vector<std::int32_t>,
                           std::vector<std::uint32_t>, std::vector<double>>;
@@ -30,13 +38,23 @@ using VVec =
                  ROOT::RNTupleView<std::int32_t>,
                  ROOT::RNTupleView<std::uint32_t>, ROOT::RNTupleView<double>>;
 
-std::vector<std::pair<std::string, std::string>>
+enum FieldTypes { String, Int32, Double };
+
+std::vector<std::pair<std::string, enum FieldTypes>>
 GetFieldNamesAndTypes(const ROOT::REntry &entry) {
-  std::vector<std::pair<std::string, std::string>> fields;
+  std::vector<std::pair<std::string, enum FieldTypes>> fields;
 
   for (const auto &val : entry) {
-    fields.emplace_back(val.GetField().GetFieldName(),
-                        val.GetField().GetTypeName());
+    std::string typeName = val.GetField().GetTypeName();
+    if (typeName == "std::string") {
+      fields.emplace_back(val.GetField().GetFieldName(), FieldTypes::String);
+    } else if (typeName == "std::int32_t") {
+      fields.emplace_back(val.GetField().GetFieldName(), FieldTypes::Int32);
+    } else if (typeName == "double") {
+      fields.emplace_back(val.GetField().GetFieldName(), FieldTypes::Double);
+    } else {
+      throw std::runtime_error("Unsupported fieldtype: " + typeName);
+    }
   }
 
   std::sort(fields.begin(), fields.end(),
@@ -58,12 +76,10 @@ inline void ProcessStringField(std::ostream &out, std::string_view fieldName,
   out << '"' << fieldName << "\":\"" << view(entryId) << '"';
 }
 
-enum FieldTypes { String, Int32, Uint32, Double };
-
 int main(int argc, char **argv) {
-  if (argc < 3) {
-    std::cerr << "Usage: " << argv[0] << " <input.ntuple.root> <RNTuple name>"
-              << std::endl;
+  if (argc < 4) {
+    std::cerr << "Usage: " << argv[0]
+              << " <input.ntuple.root> <RNTuple name> <scale>" << std::endl;
     return 1;
   }
   const char *kNTupleFileName = argv[1];
@@ -71,77 +87,76 @@ int main(int argc, char **argv) {
 
   auto reader = ROOT::RNTupleReader::Open(kNTupleName, kNTupleFileName);
   auto fields = GetFieldNamesAndTypes(reader->GetModel().GetDefaultEntry());
+  reader->GetModel();
 
   std::vector<std::pair<std::string, int>> fieldMap;
   std::vector<FVec> fieldsVec;
   std::vector<VVec> viewVec;
-  std::cout << "Initializing arrays…" << std::endl;
+  std::cout << "Initializing the view arrays…" << std::endl;
 
   for (const auto &[fieldName, fieldType] : fields) {
-    if (fieldType == "std::string") {
+    switch (fieldType) {
+    case String:
       fieldsVec.emplace_back(std::in_place_type<std::vector<std::string>>,
                              reader->GetNEntries());
       viewVec.emplace_back(reader->GetView<std::string>(fieldName));
       fieldMap.emplace_back(fieldName, FieldTypes::String);
-    } else if (fieldType == "std::int32_t") {
+      break;
+    case Int32:
       fieldsVec.emplace_back(std::in_place_type<std::vector<std::int32_t>>,
                              reader->GetNEntries());
       viewVec.emplace_back(reader->GetView<std::int32_t>(fieldName));
       fieldMap.emplace_back(fieldName, FieldTypes::Int32);
-    } else if (fieldType == "std::uint32_t") {
-      std::cout << "uint!\n";
-      fieldsVec.emplace_back(std::in_place_type<std::vector<std::uint32_t>>,
-                             reader->GetNEntries());
-      viewVec.emplace_back(reader->GetView<std::uint32_t>(fieldName));
-      fieldMap.emplace_back(fieldName, FieldTypes::Uint32);
-    } else if (fieldType == "double") {
+    case Double:
       fieldsVec.emplace_back(std::in_place_type<std::vector<double>>,
                              reader->GetNEntries());
       viewVec.emplace_back(reader->GetView<double>(fieldName));
       fieldMap.emplace_back(fieldName, FieldTypes::Double);
-    } else {
-      std::cerr << "Found an unsupported fieldtype: " << fieldType << std::endl;
-      return 1;
+      break;
+    default:
+      throw std::runtime_error("Found an unsupported fieldtype.");
     }
   }
 
-  std::cout << "Reading RNTuple into memory…" << std::endl;
+  std::cout << "Initializing the writer…" << std::endl;
+  auto model = ROOT::RNTupleModel::Create();
 
-  for (size_t field = 0; field < fieldsVec.size(); ++field) {
-    const auto &[fieldName, fieldType] = fields[field];
-    std::visit(
-        [&](auto &dstVec, auto &srcView) {
-          using D = std::decay_t<decltype(dstVec)>;
-          using Elem =
-              std::remove_const_t<std::remove_reference_t<decltype(srcView(
-                  (ROOT::NTupleSize_t)0))>>;
-          if constexpr (std::is_same_v<typename D::value_type, Elem>) {
-            for (size_t i = 0; i < reader->GetNEntries(); ++i) {
-              dstVec[i] = srcView(static_cast<ROOT::NTupleSize_t>(i));
-            }
-          }
-        },
-        fieldsVec[field], viewVec[field]);
-  }
-
-  std::cout << "Converting to JSON…" << std::endl;
-
-  std::vector<std::string> fieldNames;
   for (const auto &[fieldName, fieldType] : fields) {
-    fieldNames.emplace_back(fieldName);
-  }
-  std::ofstream ofs(std::string(kNTupleName) + ".ndjson");
-  for (size_t i = 0; i < reader->GetNEntries(); ++i) {
-    json obj = json::object();
-    for (size_t f = 0; f < fieldNames.size(); ++f) {
-      const auto &key = fieldNames[f];
-      std::visit([&](auto &vec) { obj[key] = vec[i]; }, fieldsVec[f]);
+    switch (fieldType) {
+    case String:
+      model->MakeField<std::string>(fieldName);
+      break;
+    case Int32:
+      model->MakeField<std::int32_t>(fieldName);
+      break;
+    case Double:
+      model->MakeField<double>(fieldName);
+      break;
+    default:
+      throw std::runtime_error("Found an unsupported fieldtype.");
     }
-    ofs << obj.dump() << "\n";
   }
+  auto options = ROOT::RNTupleWriteOptions();
+  auto writer = ROOT::RNTupleWriter::Recreate(std::move(model), kNTupleName,
+                                              kNTupleFileName, options);
 
-  std::cout << "Done converting RNTuple to JSON!" << std::endl;
+  // auto writer = ROOT::RNTupleWriter::Recreate(std::move(reader->GetModel()),
+  // kNTupleName, "test.root.rntuple");
 
-  return 0;
+  // for (size_t field = 0; field < fieldsVec.size(); ++field) {
+  //   const auto &[fieldName, fieldType] = fields[field];
+  //   std::visit(
+  //       [&](auto &dstVec, auto &srcView) {
+  //         using D = std::decay_t<decltype(dstVec)>;
+  //         using Elem =
+  //             std::remove_const_t<std::remove_reference_t<decltype(srcView(
+  //                 (ROOT::NTupleSize_t)0))>>;
+  //         if constexpr (std::is_same_v<typename D::value_type, Elem>) {
+  //           for (size_t i = 0; i < reader->GetNEntries(); ++i) {
+  //             dstVec[i] = srcView(static_cast<ROOT::NTupleSize_t>(i));
+  //           }
+  //         }
+  //       },
+  //       fieldsVec[field], viewVec[field]);
+  // }
 }
-
